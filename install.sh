26c77d67c11450c4cc8c4ee3b91eab77ae4f5cce
Romain Salles <rmnsalles@gmail.com>
2016-01-15 01:28:45 +0100

    Add instructions to install my environment

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# ========================== #
+# Install my dev environment #
+# ========================== #
+
+# Configure iTerm2
+# ==============================================================================
+echo "Download iTerm2"
+open https://www.iterm2.com/
+
+# Display hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles 1
+echo "Puis relancer Finder : Menu Pomme –> Forcer à quitter –> Finder –> Relancer"
+
+# Use Solarized theme
+echo "File downloaded at https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized"
+echo "Use this Preset : ./Solarized Dark.itermcolors"
+
+# XCode
+# ==============================================================================
+echo "Start by downloading and installing XCode"
+echo "Don't forget to open it to accept the license and install the different components"
+
+# Install Homebrew
+# ==============================================================================
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Git
+# ==============================================================================
+brew install git
+git config --global user.email "rmnsalles@gmail.com"
+git config --global user.name "Romain Salles"
+
+# nvim
+# ==============================================================================
+brew install neovim/neovim/neovim
+mkdir ~/.config/nvim/colors/
+cp solarized.vim ~/.config/nvim/colors/
+mkdir ~/.config
+mkdir ~/.config/nvim
+mv init.vim ~/.config/nvim/init.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Dans nvim, executer :PlugInstall
+brew install the_silver_searcher
+
+# Rvm & Ruby
+# ==============================================================================
+# Install rvm
+curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --rails
+source ~/.rvm/scripts/rvm
+# Install ruby
+rvm install ruby-2.3.0
+rvm docs generate-ri
+# Make sure you have the latest version of RVM
+rvm get stable --autolibs=enable --auto-dotfiles
+# To see all the versions of Ruby installed via RVM
+rvm list rubies
+# To use a specific version
+rvm use 2.3.0 --default
+# To check
+ruby -v
